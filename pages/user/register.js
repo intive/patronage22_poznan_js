2bd1b6b72d2e5925b08c9b3d814f839b3e2610d4
wolnio <kowolniak@gmail.com>
2022-01-25 13:04:30 +0100

    feat: Sign up form P2022-363

--- pages/user/register.js
+++ pages/user/register.js
@@ -1,7 +1,125 @@
-export default function Home() {
+import { useState } from "react/cjs/react.development";
+import { Input, FormFlex } from "components/Form";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 50%;
+  margin: 50px auto;
+  background-color: ${(props) => (props.theme === "light" ? "#fff" : "#666")};
+`;
+
+export default function UserRegister() {
+  const initValues = {
+    username: "",
+    email: "",
+    confirmEmail: "",
+    password: "",
+  };
+  const [formValues, setFormValues] = useState(initValues);
+  const [formErrors, setFormErrors] = useState(initValues);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmin = (e) => {
+    e.preventDefault();
+
+    setFormErrors(validate(formValues));
+    console.log(formErrors);
+    if(Object.keys(formErrors).length === 0){
+      console.log(formValues);
+    }
+  };
+
+  const validate = (values) => {
+    const errors = {};
+
+    /* Username validation
+     */
+    if (!values.username) {
+      errors.username = "Username is required";
+    } else if (values.username.length < 6) {
+      errors.username = "Username must be at least 6 characters!";
+    } else if (values.username.length > 20) {
+      errors.username = "Username must be 20 characters or less!";
+    } else if (/\W/.test(values.username)) {
+      errors.username = "Only characters and digits allowed!";
+    }
+
+    /* Email validation
+     */
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(values.email)) {
+      errors.email = "Not valid format!";
+    }
+
+    /* confirm email validation
+     */
+    if (values.email !== values.confirmEmail) {
+      errors.confirmEmail = "Emails must be the same!";
+    }
+
+    /* Password validation
+     */
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 6) {
+      errors.password = "Password must be at least 6 characters!";
+    }
+
+    return errors;
+  };
+
   return (
-    <div >
-        user register
-    </div>
-  )
+    <Wrapper theme={"light"}>
+      <FormFlex>
+        <Input
+          label="Username"
+          id="username"
+          name="username"
+          type="text"
+          value={formValues.username}
+          onInputChange={handleChange}
+          error={formErrors.username}
+        />
+
+        <Input
+          label="Email"
+          id="email"
+          name="email"
+          type="email"
+          value={formValues.email}
+          onInputChange={handleChange}
+          error={formErrors.email}
+        />
+
+        <Input
+          label="Confirm email"
+          id="confirmEmail"
+          name="confirmEmail"
+          type="email"
+          value={formValues.confirmEmail}
+          onInputChange={handleChange}
+          error={formErrors.confirmEmail}
+        />
+
+        <Input
+          label="Password"
+          id="password"
+          name="password"
+          type="pasword"
+          value={formValues.password}
+          onInputChange={handleChange}
+          error={formErrors.password}
+        />
+
+        <button onClick={handleSubmin}>Sign up</button>
+        <pre>{/* JSON.stringify(formValues, undefined, 2) */}</pre>
+      </FormFlex>
+    </Wrapper>
+  );
 }
