57b4dc20bbd6cd7f6177388b0be3afe95531a05b
Maciej Stępień <maciej.stepien1994gmail.com>
2022-02-01 13:41:14 +0100

    refactor registration form and fix error

--- app/components/Form/index.js
+++ app/components/Form/index.js
@@ -1,22 +1,41 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export function Input(props) {
-    return (
-        <>
-            <label htmlFor={props.id}>{props.label}</label>
-            <input
-                id={props.id}
-                name={props.name}
-                type={props.type}
-                value={props.value}
-                onChange={props.onInputChange} />
-            {props.error && <div style={{ color: "red"}}>{props.error}</div>}
-        </>
-    )
+  return (
+    <>
+      <label htmlFor={props.name}>{props.label}</label>
+      <input
+        id={props.name}
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        onChange={props.onInputChange}
+      />
+      {props.error && <div style={{ color: "red" }}>{props.error}</div>}
+    </>
+  );
+}
+
+export function Button(props) {
+  return (
+    <button onClick={props.click} type={props.type}>
+      {props.name}
+    </button>
+  );
 }
 
 export const FormFlex = styled.form`
-    display: flex;
-    flex-direction: column; 
-    max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
 `;
+
+export const Wrapper = styled.div`
+  width: 50%;
+  margin: 50px auto;
+  background-color: ${(props) => (props.theme === "light" ? "#fff" : "#666")};
+`;
+
+export const UserReg = /^[A-z0-9]\w{3,10}$/;
+export const PassReg = /[!@#]/;
+export const EmailReg = /^[-\w.]+@([-\w]+\.)+[a-z]+$/i;
--- pages/user/register.js
+++ pages/user/register.js
@@ -1,108 +1,241 @@
-import { Input, FormFlex } from "components/Form/index";
 import { useState } from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  width: 50%;
-  margin: 50px auto;
-  background-color: ${(props) => (props.theme === "light" ? "#fff" : "#666")};
-`;
+import {
+  Input,
+  Button,
+  FormFlex,
+  Wrapper,
+  UserReg,
+  PassReg,
+  EmailReg,
+} from "components/Form/index";
 
 export default function UserRegister() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [confirmEmail, setConfirmEmail] = useState("");
+  const inputProperty = {
+    username: "username",
+    password: "password",
+    email: "email",
+    confirmEmail: "confirmEmail",
+    type: {
+      email: "email",
+      text: "text",
+      password: "password",
+    },
+    label: {
+      username: "User Name",
+      password: "Your password",
+      email: "Email address",
+      confirmEmail: "Confirm your Email address",
+    },
+  };
+  const initialState = {
+    username: "",
+    password: "",
+    email: "",
+    confirmEmail: "",
+  };
 
-  const [usernameError, setUsernameError] = useState("");
-  const [userPasswordError, setUserPasswordError] = useState("");
-  const [userEmailError, setUserEmailError] = useState("");
-  const [userConfirmEmailError, setUserConfirmEmailError] = useState("");
+  const initialErrors = {
+    userError: "",
+    passwordError: "",
+    emailError: "",
+    confirmEmailError: "",
+  };
 
+  const errMessage = {
+    requiredField: "The field is required",
+    minLengthUserInput: "Min 4 characters",
+    minLengthPassInput: "Min 8 characters",
+    maxLeghthInput: "Max 20 characters",
+    incorrectEmail: "E-mail must contain ' @ '",
+    strongPass:
+      "Use strong password. You can use special character like a @ ! #",
+    userFormChar: "use characters from [a-Z , 0-9]",
+    emailEmailFormError: "Check your email!",
+    comparisonEmailsError: "Email address must be the same!",
+    noError: "",
+  };
+
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState(initialErrors);
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+    inputValidationLive(value, name);
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+  const inputValidationLive = (inputValue, inputName) => {
+    const {
+      requiredField,
+      minLengthUserInput,
+      minLengthPassInput,
+      maxLeghthInput,
+      incorrectEmail,
+      strongPass,
+      userFormChar,
+      emailEmailFormError,
+      noError,
+    } = errMessage;
+
+    switch (inputName) {
+      case inputProperty.username: {
+        if (inputValue === "") {
+          setErrors({ ...errors, userError: requiredField });
+        } else if (inputValue.length < 4) {
+          setErrors({ ...errors, userError: minLengthUserInput });
+        } else if (inputValue.length > 20) {
+          setErrors({ ...errors, userError: maxLeghthInput });
+        } else if (!UserReg.test(inputValue)) {
+          setErrors({ ...errors, userError: userFormChar });
+        } else {
+          setErrors({ ...errors, userError: noError });
+        }
+        break;
+      }
+      case inputProperty.password: {
+        if (inputValue === "") {
+          setErrors({ ...errors, passwordError: requiredField });
+        } else if (inputValue.length < 8) {
+          setErrors({
+            ...errors,
+            passwordError: minLengthPassInput,
+          });
+        } else if (inputValue.length > 20) {
+          setErrors({ ...errors, passwordError: maxLeghthInput });
+        } else if (!PassReg.test(inputValue)) {
+          setErrors({ ...errors, passwordError: strongPass });
+        } else {
+          setErrors({ ...errors, passwordError: noError });
+        }
+        break;
+      }
+      case inputProperty.email: {
+        if (inputValue === "") {
+          setErrors({ ...errors, emailError: requiredField });
+        } else if (inputValue.indexOf("@") === -1) {
+          setErrors({ ...errors, emailError: incorrectEmail });
+        } else if (!EmailReg.test(inputValue)) {
+          setErrors({ ...errors, emailError: emailEmailFormError });
+        } else {
+          setErrors({ ...errors, emailError: noError });
+        }
+        break;
+      }
+      case inputProperty.confirmEmail: {
+        if (inputValue === "") {
+          setErrors({
+            ...errors,
+            confirmEmailError: requiredField,
+          });
+        } else if (inputValue.indexOf("@") === -1) {
+          setErrors({ ...errors, confirmEmailError: incorrectEmail });
+        } else if (!EmailReg.test(inputValue)) {
+          setErrors({ ...errors, confirmEmailError: emailEmailFormError });
+        } else {
+          setErrors({ ...errors, confirmEmailError: noError });
+        }
+        break;
+      }
+    }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    setUsernameError("");
-    setUserPasswordError("");
-    setUserEmailError("");
-    setUserConfirmEmailError("");
-
-    if (username === "") {
-      setUsernameError("username is required");
-    } else if (username.length < 5) {
-      setUsernameError("username should be a min 5 characters");
-    }
-    if (password === "") {
-      setUserPasswordError("password is required");
-    } else if (password.length < 8) {
-      setUserPasswordError("password should be a min 8 characters");
-    }
-
-    if (email === "") {
-      setUserEmailError("email is required!");
-    } else if (email.indexOf("@") === -1) {
-      setUserEmailError("check your email adress!");
-    }
-
-    if (confirmEmail === "") {
-      setUserConfirmEmailError("email is required!");
-    } else if (confirmEmail.indexOf("@") === -1) {
-      setUserConfirmEmailError("check your email adress!");
-    }
-
-    if (email !== confirmEmail) {
-      setUserEmailError("email must be the same!");
-      setUserConfirmEmailError("email must be the same!");
-    }
-
-    if (username && password && email && confirmEmail) {
-      const newUser = { username, password, email };
+    const validation = formValidation();
+    if (validation.correct) {
+      setValues(initialState);
+      setErrors(initialErrors);
+      const newUser = {
+        username: values.username,
+        password: values.password,
+        email: values.email,
+      };
       console.log(newUser);
+    } else {
+      setErrors({
+        ...errors,
+      });
     }
+  };
+
+  const formValidation = () => {
+    let username = false;
+    let password = false;
+    let email = false;
+    let confirmEmail = false;
+    let correct = false;
+    let comparisonEmails = false;
+
+    if (UserReg.test(values.username)) {
+      username = true;
+    }
+    if (values.password.length > 8) {
+      password = true;
+    }
+
+    if (EmailReg.test(values.email)) {
+      email = true;
+    }
+
+    if (EmailReg.test(values.email)) {
+      confirmEmail = true;
+    }
+
+    if (values.email !== values.confirmEmail) {
+      setErrors({
+        ...errors,
+        emailError: errMessage.comparisonEmailsError,
+        confirmEmailError: errMessage.comparisonEmailsError,
+      });
+    } else {
+      comparisonEmails = true;
+    }
+
+    // TODO nie działa ustawianie errora w tej funkcji.
+
+    if (username && password && email && confirmEmail && comparisonEmails) {
+      correct = true;
+    }
+    return { username, password, email, correct };
   };
 
   return (
     <Wrapper theme={"light"}>
       <FormFlex>
         <Input
-          id="username"
-          name="username"
-          type="text"
-          label="Username"
-          value={username}
-          error={usernameError}
-          onInputChange={(e) => setUsername(e.target.value)}
+          name={inputProperty.username}
+          type={inputProperty.type.text}
+          label={inputProperty.label.username}
+          value={values.username}
+          error={errors.userError}
+          onInputChange={handleOnChange}
         />
         <Input
-          id="password"
-          name="password"
-          type="password"
-          label="Password"
-          value={password}
-          error={userPasswordError}
-          onInputChange={(e) => setPassword(e.target.value)}
+          name={inputProperty.password}
+          type={inputProperty.type.password}
+          label={inputProperty.label.password}
+          value={values.password}
+          error={errors.passwordError}
+          onInputChange={handleOnChange}
         />
         <Input
-          id="email"
-          name="email"
-          type="text"
-          label="Email"
-          value={email}
-          error={userEmailError}
-          onInputChange={(e) => setEmail(e.target.value)}
+          name={inputProperty.email}
+          type={inputProperty.type.email}
+          label={inputProperty.label.email}
+          value={values.email}
+          error={errors.emailError}
+          onInputChange={handleOnChange}
         />
         <Input
-          id="Confirmemail"
-          name="Confirmemail"
-          type="text"
-          label="Confirmemail"
-          value={confirmEmail}
-          error={userConfirmEmailError}
-          onInputChange={(e) => setConfirmEmail(e.target.value)}
+          name={inputProperty.confirmEmail}
+          type={inputProperty.type.email}
+          label={inputProperty.label.confirmEmail}
+          value={values.confirmEmail}
+          error={errors.confirmEmailError}
+          onInputChange={handleOnChange}
         />
-        <button onClick={handleSubmit} type="submit">
-          Register
-        </button>
+        <Button type="submit" name="Register" click={handleSubmit} />
       </FormFlex>
     </Wrapper>
   );
