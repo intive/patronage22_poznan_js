c3f6ea89dfd8a23e9e7ae63882c59583abb4d2fd
Diana Szałek <diana.szalek@intive.com>
2022-02-01 15:21:12 +0100

    test: setup for tests #P2022-358

---
+++ app/components/Form/__tests__/Form.spec.js
@@ -0,0 +1,12 @@
+import render, { screen } from '../../../../testUtils/setup'; // TODO add aliases
+
+import { Input } from '../index';
+
+describe('Input', () => {
+    it('renders given label', () => {
+        const label = 'My input';
+        render(<Input label={label}/>);
+
+        expect(screen.getByText(label)).toBeVisible();
+    })
+})
