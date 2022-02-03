e7af84a26a2bf63e291ef82d4348119bc1ea0caa
piotr pieczara <piotr.pieczara@gmail.com>
2022-02-04 00:57:18 +0100

    move import and configuration to separate file

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import Layout from 'components/Layout';
 import '../styles/globals.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return <Layout><Component {...pageProps} /></Layout>
