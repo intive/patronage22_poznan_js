4f638ca7bb11a201464acb9b5ebab094d2e96445
piotr pieczara <piotr.pieczara@gmail.com>
2022-02-04 19:49:50 +0100

    remove changes to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
-import '../styles/fontawesome.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRuler } from '@fortawesome/free-solid-svg-icons';
-import { faYenSign } from '@fortawesome/free-solid-svg-icons';
-
 
 export default function Home() {
   return (
@@ -19,12 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to inTiVi
-          <FontAwesomeIcon icon={faRuler} />  
-          <FontAwesomeIcon icon={faYenSign} />  
-
         </h1>
-        <FontAwesomeIcon icon={faRuler} />  
-
         <p className={styles.description}>
           Please <Link href={'/user/login'}>sign in</Link>.
         </p>
