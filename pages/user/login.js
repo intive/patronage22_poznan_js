046b4995c39fe1b8a90728356ffbdd63e9c7971e
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-02-07 17:32:29 +0100

    feat: login page styles

--- pages/user/login.js
+++ pages/user/login.js
@@ -1,15 +1,51 @@
-import { Input, FormFlex } from 'components/Form/index'; 
+import { Input } from 'components/Form/index';
 import { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-    width: 50%;
-    margin: 50px auto;
-    background-color: ${(props) => props.theme === 'light' ? '#fff' : '#666'};
+    margin-left: 35vw;
+    margin-top: -7vh;
+    width: 30vw;
+    height: 86vh;
+    color: rgb(255, 255, 255);
+    background-color: rgba(0, 0, 0, 0.8);
+`;
+
+const LoginPageContainer = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('../UserLoginBackgroundImg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position:center;
+    z-index: -2;
+`;
+
+const Header = styled.div`
+   padding-left: 3vw;
+   padding-top: 1.5vh;
+   z-index: 3;
+`;
+
+const LayerWrapper = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    z-index:-1;
+`;
+
+const FormContainer = styled.div`
+    padding-left: 4vw;
+    padding-top: 2vh;
+    width: 18vw;
+    height: 68vh;
 `;
 
 export default function UserLogin() {
-
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [usernameError, setUsernameError] = useState('');
@@ -26,24 +62,31 @@ export default function UserLogin() {
     }
 
     return (
-        <Wrapper theme={"light"}>
-            <FormFlex>
-                <Input
-                    id="username"
-                    name="username"
-                    type="text"
-                    label="Username"
-                    value={username}
-                    error={usernameError}
-                    onInputChange={(e) => setUsername(e.target.value)} />
-                <Input
-                    name="password"
-                    type="password"
-                    label="Password"
-                    value={password}
-                    onInputChange={(e) => setPassword(e.target.value)} />
-                <button onClick={userLogin}>Sign In</button>
-            </FormFlex>
-        </Wrapper>
+        <LoginPageContainer>
+            <LayerWrapper></LayerWrapper>
+            <Header>
+                <img src='../logo-intivi.svg'></img>
+            </Header>
+            <Wrapper>
+                <FormContainer>
+                    <h1 style={{fontWeight:"600"}}>Sign In</h1>
+                    <Input
+                        id="username"
+                        name="username"
+                        type="text"
+                        label="Username"
+                        value={username}
+                        error={usernameError}
+                        onInputChange={(e) => setUsername(e.target.value)} />
+                    <Input
+                        name="password"
+                        type="password"
+                        label="Password"
+                        value={password}
+                        onInputChange={(e) => setPassword(e.target.value)} />
+                    <button onClick={userLogin}>Sign In</button>
+                </FormContainer>
+            </Wrapper>
+        </LoginPageContainer>
     )
-}
+};
