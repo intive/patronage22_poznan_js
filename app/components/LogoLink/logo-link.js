8194e248a33b4e08fc33e1f7fb355d180e55f9c1
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-02-08 21:41:02 +0100

    feat: Add `LogoLink` component

---
+++ app/components/LogoLink/logo-link.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+import appLogoImg from "../../../public/app-logo.svg";
+
+const LogoLink = () => {
+  return (
+    <Link href="/" passHref>
+      <StyledLogoLink>
+        <Image src={appLogoImg} alt="InTiVi logo" />
+      </StyledLogoLink>
+    </Link>
+  );
+};
+
+export default LogoLink;
+
+const StyledLogoLink = styled.a`
+  width: clamp(90px, 20vw, 134px);
+  display: flex;
+`;
