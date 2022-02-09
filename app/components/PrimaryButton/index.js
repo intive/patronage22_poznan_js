0e4b2a31cc198c5c9f5d9034d8c6b48ab7ee1517
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-02-09 23:13:01 +0100

    refactor: change export component to default

--- app/components/PrimaryButton/index.js
+++ app/components/PrimaryButton/index.js
@@ -1,12 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const PrimaryButton = styled.button`
+const PrimaryButton = styled.button`
   color: #fff;
   font-size: 1rem;
   text-transform: capitalize;
   background-color: #db0510;
-  padding: .6rem 1.2rem;
+  padding: 0.6rem 1.2rem;
   border: none;
   border-radius: 3px;
   white-space: nowrap;
-`
+`;
+
+export default PrimaryButton;
