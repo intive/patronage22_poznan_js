3053c55b3c321b93615a79ccd713fe15710ab372
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-02-11 21:29:30 +0100

    feat: create media queries

---
+++ app/consts/mediaQueries.js
@@ -0,0 +1,9 @@
+const size = {
+  tablet: '768px',
+  desktop: '1024px',
+};
+
+export const device = {
+  tablet: `@media (min-width: ${size.tablet})`,
+  desktop: `@media (min-width: ${size.desktop})`,
+};
