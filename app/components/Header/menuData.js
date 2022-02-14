a41147a6da1a134bf747443886db25d22f792dd6
Patryk Sagan <patryksagan15@wp.pl>
2022-02-14 14:28:16 +0100

    fix: bug with link directory

--- app/components/Header/menuData.js
+++ app/components/Header/menuData.js
@@ -1,9 +1,9 @@
 export const navData = [
-  { id: 1, name: 'home', link: '' },
-  { id: 2, name: 'tv shows', link: '' },
-  { id: 3, name: 'movies', link: '' },
-  { id: 4, name: 'new & popular', link: '' },
-  { id: 5, name: 'my list', link: '' },
+  { id: 1, name: 'home', link: '/' },
+  { id: 2, name: 'tv shows', link: '/' },
+  { id: 3, name: 'movies', link: '/' },
+  { id: 4, name: 'new & popular', link: '/' },
+  { id: 5, name: 'my list', link: '/' },
 ];
 export const userImg = ':)';
 export const mobileMenu = 'Explore';
