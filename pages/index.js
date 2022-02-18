44412bdeed13a7ec67fdd1cfe782b9e29f5a4dda
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2022-02-18 14:04:17 +0100

    refactor: move actions/app to example, revert other changes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
-import { Modal } from 'components/Modal';
-import { Button } from 'components/SuperButton';
-import { useActions } from 'actions/app';
-
-const initialState = {
-  isModalOpen: false,
-  modalContent: '',
-};
 
 export default function Home() {
-  const appState = useActions(initialState);
-
   return (
     <div className={styles.container}>
-      {appState.isModalOpen && <Modal content={appState.modalContent} />}
       <Head>
         <title>inTiVi 2022</title>
         <meta name="description" content="inTiVi 2022" />
@@ -29,7 +18,6 @@ export default function Home() {
           Please <Link href={'/user/login'}>sign in</Link>.
         </p>
         <br />
-        <Button />
       </main>
 
       <footer className={styles.footer}>(c) intive 2022</footer>
