e00f5a0078fba23c353a84a2a3cc04b02f2de823
Maciek <maciej.stepien1994@gmail.com>
2022-02-20 22:17:48 +0100

    feat: swagger doc for API

---
+++ pages/doc/api-doc.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import { createSwaggerSpec } from 'next-swagger-doc';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+
+const ApiDoc = ({ spec }) => {
+  return <SwaggerUI spec={spec} />;
+};
+
+export const getStaticProps = async () => {
+  const spec = createSwaggerSpec({
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'NextJS Swagger',
+        version: '0.1.0',
+      },
+    },
+  });
+  return {
+    props: {
+      spec,
+    },
+  };
+};
+
+export default ApiDoc;
