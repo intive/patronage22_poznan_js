fb6f5d9b49b6ff4c0bc4680e269282be2c848d6c
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2022-02-20 19:42:01 +0100

    fix: layout of LogoLink component

--- app/components/LogoLink/index.js
+++ app/components/LogoLink/index.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 import Link from 'next/link';
 import Image from 'next/image';
-import appLogoImg from '../../../public/app-logo.svg';
+import appLogoImg from '/public/app-logo.svg';
 
 const LogoLink = () => {
   return (
     <Link href="/" passHref>
       <StyledLogoLink>
-        <Image src={appLogoImg} alt="InTiVi logo" />
+        <Image
+          src={appLogoImg}
+          alt="InTiVi logo"
+          width="100%"
+          height="100%"
+          layout="responsive"
+          objectFit="contain"
+        />
       </StyledLogoLink>
     </Link>
   );
@@ -17,5 +24,6 @@ export default LogoLink;
 
 const StyledLogoLink = styled.a`
   width: clamp(90px, 20vw, 134px);
-  display: flex;
+  display: inline-block;
+  position: relative;
 `;
