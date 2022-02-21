31b2ae5008cb96982f7317fb0a6a1dd0ab26d2db
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-02-21 13:20:37 +0100

    fix: delete payload from logOut

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -9,12 +9,13 @@ export default function Layout({ children }) {
   const user = useActions({});
   return (
     <>
+      {/* It's only a temporary solution for development purposes */}
       {user.username ? <Header /> : <SimpleHeader></SimpleHeader>}
       <div style={{ position: 'absolute', bottom: '0', right: '0' }}>
         <Button primary value="Log In" onClick={() => logIn({ username: 'Grażyna' })}>
           Log In
         </Button>
-        <Button primary value="Log Out" onClick={() => logOut({})}>
+        <Button primary value="Log Out" onClick={() => logOut()}>
           Log Out
         </Button>
       </div>
