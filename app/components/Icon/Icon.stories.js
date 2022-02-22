3fd742e9e68511d8a9f2c13dc32b8d721f8f10e2
piotr pieczara <piotr.pieczara@gmail.com>
2022-02-22 02:55:24 +0100

    feat: add additional stories

--- app/components/Icon/Icon.stories.js
+++ app/components/Icon/Icon.stories.js
@@ -3,8 +3,38 @@ import Icon from '.';
 export default {
   title: 'UI/Icon',
   component: Icon,
+  argTypes: {
+    size: {
+      description: 'Sets size in relation to current font-size. ',
+    },
+  },
 };
 
 const Template = (args) => <Icon {...args} />;
 
-export const Default = Template.bind({});
+export const Custom = Template.bind({});
+
+Custom.args = {};
+
+export const WhiteBackground = Template.bind({});
+
+WhiteBackground.args = {
+  blackBackground: false,
+};
+
+export const BlackBackground = Template.bind({});
+
+BlackBackground.args = {
+  blackBackground: true,
+};
+
+export const Spin = Template.bind({});
+
+Spin.args = {
+  spin: true,
+};
+export const Pulse = Template.bind({});
+
+Pulse.args = {
+  pulse: true,
+};
--- app/components/Icon/Icon.styles.js
+++ app/components/Icon/Icon.styles.js
@@ -1,5 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const StyledIcon = styled.span``;
+const StyledIcon = styled.span`
+  border-radius: 3px;
+  display: flex;
+
+  color: #000;
+  background-color: #fff;
+
+  ${({ customBackground }) =>
+    customBackground &&
+    css`
+      background-color: ${({ customBackground }) => customBackground || '#fff'};
+    `}
+  ${({ blackBackground }) =>
+    blackBackground &&
+    css`
+      color: #fff;
+      background-color: #000;
+    `}
+`;
 
 export default StyledIcon;
