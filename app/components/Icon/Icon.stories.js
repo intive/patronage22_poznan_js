edcfef9271fd56a4b37bb19f68b9de1ecf4d8d21
piotr pieczara <piotr.pieczara@gmail.com>
2022-02-23 23:20:09 +0100

    refactor: changes in stories

--- app/components/Icon/Icon.stories.js
+++ app/components/Icon/Icon.stories.js
@@ -16,59 +16,130 @@ export default {
         step: 0.1,
       },
     },
+    rotation: {
+      defaultValue: 0,
+      control: {
+        type: 'range',
+        min: 0,
+        max: 270,
+        step: 90,
+      },
+    },
   },
 };
-const Bckg = ({ children, style }) => (
-  <div style={{ 'background-color': 'transparent', padding: '10px', marginRight: '5px', ...style }}>
+const ShowcaseContainer = ({ children, style }) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      fontSize: '2rem',
+      ...style,
+    }}
+  >
+    {children}
+  </div>
+);
+const ShowcaseRow = ({ children, style }) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      ...style,
+    }}
+  >
+    {children}
+  </div>
+);
+
+const ShowcaseBckg = ({ children, style }) => (
+  <div
+    style={{
+      backgroundColor: 'transparent',
+      padding: '10px',
+      marginRight: '5px',
+      verticalAlign: 'center',
+      ...style,
+    }}
+  >
+    {children}
+  </div>
+);
+const ShowcaseBlackBckg = ({ children, style }) => (
+  <div
+    style={{
+      backgroundColor: 'black',
+      color: 'white',
+      padding: '10px',
+      marginRight: '5px',
+      ...style,
+    }}
+  >
+    {children}
+  </div>
+);
+const ShowcaseWhiteBckg = ({ children, style }) => (
+  <div
+    style={{
+      backgroundColor: 'white',
+      color: 'black',
+      padding: '10px',
+      marginRight: '5px',
+      ...style,
+    }}
+  >
     {children}
   </div>
 );
 
 export const Showcase = (args) => (
-  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}
-    >
-      <Bckg style={{ backgroundColor: 'black', color: 'white' }}>
+  <ShowcaseContainer>
+    <ShowcaseRow>
+      <ShowcaseBlackBckg>
         <Icon {...args} />
-      </Bckg>
-      <Bckg style={{ backgroundColor: 'white', color: 'black' }}>
+      </ShowcaseBlackBckg>
+      <ShowcaseWhiteBckg>
         <Icon {...args} />
-      </Bckg>
-      <Bckg style={{ fontSize: '2rem' }}>
+      </ShowcaseWhiteBckg>
+      <ShowcaseBckg>
         In <Icon {...args} /> text
-      </Bckg>
-    </div>
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}
-    >
-      <Bckg>
+      </ShowcaseBckg>
+    </ShowcaseRow>
+    <ShowcaseRow>
+      <ShowcaseBckg>
         1x <Icon {...args} size="1x" />
-      </Bckg>
-      <Bckg>
+      </ShowcaseBckg>
+      <ShowcaseBckg>
         2x <Icon {...args} size="2x" />
-      </Bckg>
-      <Bckg>
+      </ShowcaseBckg>
+      <ShowcaseBckg>
         4x <Icon {...args} size="4x" />
-      </Bckg>
-    </div>
+      </ShowcaseBckg>
+    </ShowcaseRow>
+  </ShowcaseContainer>
+);
+
+// const templateBckg = '';
+// const templateColor = '';
+
+const TemplateContainer = ({ children, style }) => (
+  <div
+    style={{
+      padding: '20px',
+      fontSize: '4rem',
+      ...style,
+    }}
+  >
+    {children}
   </div>
 );
 
-const Template = ({ backgroundColor = 'transparent', color = 'white', ...args }) => (
-  <div style={{ backgroundColor, color, padding: '20px', fontSize: '3rem' }}>
+const Template = ({ backgroundColor, color, ...args }) => (
+  <TemplateContainer style={{ backgroundColor, color }}>
     <Icon {...args} />
-  </div>
+  </TemplateContainer>
 );
 
 export const WhiteBackground = Template.bind({});
@@ -88,10 +159,14 @@ BlackBackground.args = {
 export const Spin = Template.bind({});
 
 Spin.args = {
+  backgroundColor: 'white',
+  color: 'black',
   spin: true,
 };
 export const Pulse = Template.bind({});
 
 Pulse.args = {
+  backgroundColor: 'white',
+  color: 'black',
   pulse: true,
 };
