70da8bf5898fbe7dcc6726e0bc7001eeb97c23e1
Marcela <62512837+marcelina-hasiak@users.noreply.github.com>
2022-02-25 22:09:48 +0100

    feat: add create account page (#38)
    * feat: add create account page
    
    * feat: add ChevronIcon to ButtonAppended
    
    * fix: add id to FloatingLabelInput
    
    * fix: fix bgImage inside CreateAccountPage
    
    * refactor: change `text` props for `content`
    
    * refactor: rename util name
    
    * refactor: change directory for Spinner
    
    * refactor: delete AppLogo

--- app/components/Button/Button.styles.js
+++ app/components/Button/Button.styles.js
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { device } from 'consts/mediaQueries';
 
 const StyledButton = styled.button`
   font-size: 1rem;
@@ -6,9 +7,11 @@ const StyledButton = styled.button`
   padding: 0.6rem 1.2rem;
   color: #000;
   background-color: ${({ backgroundColor }) => backgroundColor || '#fff'};
+  width: ${({ fullWidth }) => fullWidth && '100%'};
+  min-width: 130px;
   border: 1px solid #000;
   border-radius: 3px;
-  white-space: nowrap;
+  line-height: 1.4;
   text-align: center;
 
   :disabled {
@@ -22,6 +25,28 @@ const StyledButton = styled.button`
       color: #fff;
       background-color: #db0510;
       border: none;
+    `}
+
+  ${({ appended }) =>
+    appended &&
+    css`
+      ${device.tablet} {
+        font-size: 1.6rem;
+        height: 60px;
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        border-left: 2px solid #333;
+      }
+    `}
+
+  ${({ loader }) =>
+    loader &&
+    css`
+       {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
     `}
 `;
 
---
+++ app/components/Pages/CreateAccount/UserSignUpForm/index.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import validateSignUpFormInputs, { validateUserPassword } from 'utils/validateFormInputs';
+import { FormTitle, InputAndButtonWrapper } from '../CreateAccount.styles';
+import FloatingLabelInput from '../FloatingLabelInput';
+import Button from 'components/Button';
+import Spinner from 'components/UI/Spinner';
+
+const initialState = { password: '', email: '' };
+
+const UserSignUpForm = ({ emailValue }) => {
+  const [inputValues, setInputValues] = useState({
+    password: '',
+    email: emailValue,
+  });
+  const [errorMsg, setErrorMsg] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+  };
+
+  const getErrorMessages = () => {
+    if (emailValue !== inputValues.email) {
+      return validateSignUpFormInputs(inputValues);
+    } else {
+      const passwordErrorMsg = validateUserPassword(inputValues.password);
+      return { password: passwordErrorMsg, email: '' };
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [loading]);
+
+  const handleFormData = () => {
+    setLoading(true);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const errorMessages = getErrorMessages();
+    const isNoError = Object.values(errorMessages).every((input) => input === '');
+
+    if (!isNoError) {
+      setErrorMsg(errorMessages);
+    } else {
+      setErrorMsg(initialState);
+      console.log('Form has been sent');
+      handleFormData(inputValues);
+    }
+  };
+
+  return (
+    <form>
+      <FormTitle>Please enter password to finish creating your account.</FormTitle>
+      <InputAndButtonWrapper>
+        <FloatingLabelInput
+          name="email"
+          type="email"
+          label="Email adress"
+          value={inputValues.email}
+          errorMsg={errorMsg.email}
+          onInputChange={handleInputChange}
+          autoFocus
+          withBorder
+        />
+        <FloatingLabelInput
+          name="password"
+          type="password"
+          label="Password"
+          value={inputValues.password}
+          errorMsg={errorMsg.password}
+          onInputChange={handleInputChange}
+          withBorder
+        />
+        {!loading ? (
+          <Button onClick={handleSubmit} primary fullWidth>
+            Create account
+          </Button>
+        ) : (
+          <Button onClick={handleSubmit} primary fullWidth disabled loader>
+            <Spinner />
+          </Button>
+        )}
+      </InputAndButtonWrapper>
+    </form>
+  );
+};
+
+export default UserSignUpForm;
