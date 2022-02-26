62dde1d2b5aeb3919497105d7528bff700e5a8fe
Lena Sokolova <sokolhelena94@gmail.com>
2022-02-26 12:54:32 +0100

    fix: prettier fix

--- app/components/Carousel/NavButtonStories/index.js
+++ app/components/Carousel/NavButtonStories/index.js
@@ -1,20 +1,18 @@
 import PropTypes from 'prop-types';
 import { StyledBtn } from './NavButton.styles';
 
-const Button = ({ content, handleClick}) => {
-    return (
+const Button = ({ content, handleClick }) => {
+  return (
     <>
-      <StyledBtn 
-      onClick={handleClick}>{content}</StyledBtn>
+      <StyledBtn onClick={handleClick}>{content}</StyledBtn>
 
-      <StyledBtn 
-      onClick={handleClick}>{content}</StyledBtn>
+      <StyledBtn onClick={handleClick}>{content}</StyledBtn>
     </>
-    )
-}
+  );
+};
 
 Button.propTypes = {
-    handleClick: PropTypes.func,
+  handleClick: PropTypes.func,
 };
 
 export default Button;
