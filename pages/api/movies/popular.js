8707718b6d6646c272fe62bcc5fbc5fb3ba04dec
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2022-03-02 17:36:55 +0100

    feat: backend - movies/[id] and /popular (#54)
    * feat: backend - movies/[id] and /popular
    
    * chore: cleanup

---
+++ pages/api/movies/popular.js
@@ -0,0 +1,31 @@
+import { getMoviesPopular } from '../../../lib/services/movieDb';
+
+/**
+ * @swagger
+ * /api/movies/popular:
+ *   get:
+ *     summary: Get a list of popular movies
+ *     description: Returns a list of popular movies
+ *     responses:
+ *       200:
+ *         description: list of movies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *       404:
+ *         description: list not found (shouldn't happen)
+ */
+export default async function handler(req, res) {
+  try {
+    const { id } = req.query;
+    const movies = await getMoviesPopular(id);
+    if (!movies) {
+      return res.status(404).json([]);
+    }
+    return res.status(200).json(movies.results);
+  } catch (e) {
+    const message = e.response ? await e.response.text() : e.message;
+    return res.status(400).json(message);
+  }
+}
