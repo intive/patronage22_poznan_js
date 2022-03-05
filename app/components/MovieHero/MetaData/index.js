a0555b39d17dd554a156cc1a16a1d37d5c9d1aeb
wolnio <kowolniak@gmail.com>
2022-03-06 00:37:27 +0100

    feat: add MetaData component

--- app/components/MovieHero/MetaData/index.js
+++ app/components/MovieHero/MetaData/index.js
@@ -1,8 +1,8 @@
 import { MetaDataWrapper, MetaDataItem, MetaDataSeparator } from './MetaData.styles';
 import PropTypes from 'prop-types';
 
-export default function MetaData({...props}) {
-//NOTE: real Movie data from an API will be added later, when the upper comonent MovieHero will be edited
+export default function MetaData({ ...props }) {
+  //NOTE: real Movie data from an API will be added later, when the upper comonent MovieHero will be edited
   const movieData = {
     title: 'Fight Club',
     release_date: '1999-10-15',
