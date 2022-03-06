2d64666288391f97849ccccfbd8e68599fd60e5b
Lena Sokolova <sokolhelena94@gmail.com>
2022-03-06 13:47:32 +0100

    refactor: use destructuring in left/right properties NavButtons

--- app/components/Carousel/NavButtonStories/NavButton.styles.js
+++ app/components/Carousel/NavButtonStories/NavButton.styles.js
@@ -6,8 +6,8 @@ export const StyledBtn = styled.button`
   font-size: 20px;
   border: none;
   position: absolute;
-  left: ${({ left }) => left && '0'};
-  right: ${({ right }) => right && '0'};
+  left: ${({ left }) => (left ? 0 : 'auto')};
+  right: ${({ right }) => (right ? 0 : 'auto')};
   top: 0;
   bottom: 0;
   width: 40px;
