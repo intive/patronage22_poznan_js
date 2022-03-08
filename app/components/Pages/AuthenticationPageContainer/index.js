7d0ffa3ed693eb2fe4d7ba05380ae0f6e8dbe8f1
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-03-08 14:34:27 +0100

    fix: remove useState

--- app/components/Pages/AuthenticationPageContainer/index.js
+++ app/components/Pages/AuthenticationPageContainer/index.js
@@ -1,18 +1,14 @@
 import AuthenticationFormHeader from 'components/Form/AuthenticationFormHeader';
 import { Wrapper, FormContainer, Form } from './AuthenticationPageContainer.styles';
-import { useState } from 'react';
 
 export default function AuthenticationPageContainer({ active }) {
-  const [activeMode] = useState({ active });
   return (
     <Wrapper>
       <FormContainer>
         <Form>
-          <AuthenticationFormHeader active={activeMode.active} />
+          <AuthenticationFormHeader active={active} />
           {/* It's only a temporary console.log. Here will be sign-in and sign-up component */}
-          {activeMode.active === 'sign-in'
-            ? console.log('Sign In page')
-            : console.log('Sign Up page')}
+          {active === 'sign-in' ? console.log('Sign In page') : console.log('Sign Up page')}
         </Form>
       </FormContainer>
     </Wrapper>
