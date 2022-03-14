f23c29bb484888105f7c456d8a999b773faf5818
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-03-14 22:12:25 +0100

    feat: add onBlur handler and Input component inside form

--- app/components/Form/UserSignUpForm/index.js
+++ app/components/Form/UserSignUpForm/index.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
-import validateSignUpFormInputs from 'utils/validateFormInputs';
-import FloatingLabelInput from 'components/Pages/CreateAccount/FloatingLabelInput';
+import validateSignUpFormInputs, {
+  validateUserEmail,
+  validateUserPassword,
+  validateUserName,
+} from 'utils/validateFormInputs';
+import Input from '../Input';
 import Button from 'components/UI/Button';
 
 const initialState = { email: '', password: '', name: '' };
@@ -8,7 +12,7 @@ const initialState = { email: '', password: '', name: '' };
 const UserSignUpForm = () => {
   const [inputValues, setInputValues] = useState(initialState);
   const [errorMsg, setErrorMsg] = useState(initialState);
-  const [isFormSubmitting, setLoading] = useState(false);
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -18,8 +22,14 @@ const UserSignUpForm = () => {
     });
   };
 
+  const handleBlur = (event, errorHandler) => {
+    const { name } = event.target;
+    const error = errorHandler(inputValues[name]);
+    setErrorMsg({ ...errorMsg, [name]: error });
+  };
+
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000);
+    const timer = setTimeout(() => setIsFormSubmitting(false), 3000);
     return () => {
       clearTimeout(timer);
     };
@@ -29,7 +39,9 @@ const UserSignUpForm = () => {
     event.preventDefault();
 
     const isError = checkForErrors();
-    if (!isError) handleFormData();
+    if (!isError) {
+      handleFormData();
+    }
   };
 
   const checkForErrors = () => {
@@ -40,44 +52,48 @@ const UserSignUpForm = () => {
   };
 
   const handleFormData = async () => {
-    try {
-      const response = await fetch('/api/user', {
-        method: 'POST',
-        body: JSON.stringify(inputValues),
-      });
-      if (response.status !== 200) {
-        throw new Error(`Request failed: ${response.status}`);
-      }
-    } catch (error) {
-      console.log(`Registration failed! ${error.message}`);
-    }
+    setIsFormSubmitting(true);
+    // try {
+    //   const response = await fetch('/api/user', {
+    //     method: 'POST',
+    //     body: JSON.stringify(inputValues),
+    //   });
+    //   if (response.status !== 200) {
+    //     throw new Error(`Request failed: ${response.status}`);
+    //   }
+    // } catch (error) {
+    //   console.log(`Registration failed! ${error.message}`);
+    // }
   };
 
   return (
     <form>
-      <FloatingLabelInput
-        name="email"
+      <Input
+        id="email"
         type="email"
         label="Email adress"
         value={inputValues.email}
-        errorMsg={errorMsg.email}
+        error={errorMsg.email}
         onInputChange={handleInputChange}
+        onBlur={(e) => handleBlur(e, validateUserEmail)}
       />
-      <FloatingLabelInput
-        name="name"
+      <Input
+        id="name"
         type="text"
         label="Name"
         value={inputValues.name}
-        errorMsg={errorMsg.name}
+        error={errorMsg.name}
         onInputChange={handleInputChange}
+        onBlur={(e) => handleBlur(e, validateUserName)}
       />
-      <FloatingLabelInput
-        name="password"
+      <Input
+        id="password"
         type="password"
         label="Password"
         value={inputValues.password}
-        errorMsg={errorMsg.password}
+        error={errorMsg.password}
         onInputChange={handleInputChange}
+        onBlur={(e) => handleBlur(e, validateUserPassword)}
       />
       <Button
         isLoading={isFormSubmitting}
