5ad2b4aca3c76c560d60cc985e07dd545b02e0e9
piotr pieczara <piotr.pieczara@gmail.com>
2022-03-16 00:16:18 +0100

    feat: add category endpoint

---
+++ pages/api/movies/category/[id].js
@@ -0,0 +1,36 @@
+import { getListOfMoviesByCategoryId } from '../../../../lib/services/movieDb';
+
+/**
+ * @swagger
+ * /api/movies/category/{id}:
+ *   get:
+ *     summary: Get a list of movies belonging to given category.
+ *     description: Returns a list of movies belonging to given category.
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       required: true
+ *       description: Id of the category to get a list of movies belonging to given category.
+ *     responses:
+ *       200:
+ *         description: movie object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       404:
+ *         description: movie not found
+ */
+export default async function handler(req, res) {
+  try {
+    const { id } = req.query;
+    const movie = await getListOfMoviesByCategoryId(id);
+    if (!movie) {
+      return res.status(404).json();
+    }
+    return res.status(200).json(movie.results);
+  } catch (e) {
+    const message = e.response ? await e.response.text() : e.message;
+    return res.status(400).json(message);
+  }
+}
