dbf9aa2ff4bb3d3e76d937a4a9a2b80c4a5878a8
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2022-03-15 18:50:07 +0100

    feat: validate data on user register

--- pages/api/users/register.js
+++ pages/api/users/register.js
@@ -1,6 +1,7 @@
 import mongoClient from 'lib/mongoDb';
 import { randomUUID } from 'crypto';
 import { getPassHash } from 'lib/hash';
+import { validateUserEmail, validateUserPassword } from 'utils/validateFormInputs';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -14,6 +15,11 @@ export default async function handler(req, res) {
 
   if (!(username && password && email)) {
     return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  // validation
+  if (validateUserEmail(email) || validateUserPassword(password)) {
+    return res.status(400).json({ error: 'Invalid values for required fields.' });
   }
 
   const userByEmail = await db.collection('users').countDocuments({ email }, { limit: 1 });
