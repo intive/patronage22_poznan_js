9f6d9c66b5f04679352093059f871a193a334859
piotr pieczara <piotr.pieczara@gmail.com>
2022-03-17 00:13:49 +0100

    feat: add genre/movie/list endpoint

---
+++ pages/api/movies/genre/movie/list.js
@@ -0,0 +1,30 @@
+import { getListOfGenres } from '../../../../../lib/services/movieDb';
+
+/**
+ * @swagger
+ * /api/movies/genre/movie/list:
+ *   get:
+ *     summary: Get the list of official genres for movies
+ *     description: Returns the list of official genres for movies
+ *     responses:
+ *       200:
+ *         description: list of official genres for movies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       404:
+ *         description: list not found (shouldn't happen)
+ */
+export default async function handler(req, res) {
+  try {
+    const genres = await getListOfGenres(req);
+    if (!genres) {
+      return res.status(404).json([]);
+    }
+    return res.status(200).json(genres);
+  } catch (e) {
+    const message = e.response ? await e.response.text() : e.message;
+    return res.status(400).json(message);
+  }
+}
