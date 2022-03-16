992f13974dddbc0020fdc32e69a74b13cce368a1
P.Sagan <83120643+hiimsagan@users.noreply.github.com>
2022-03-16 16:31:45 +0100

    feat: redesign Header component (#58)
    * refactor: change component place
    and remove styles for Nav
    
    * refactor: change import
    
    * refactor: change place of Nav
    
    * feat:add icons and picture for avatar
    
    * refactor: removing unnecessary things
    
    * feat: add Action
    
    * refactor: Navigation nad add storybook
    
    * feat: add icon home
    
    * refactor: header changes
    
    * fix: import component LogoLink
    
    * fix:  background color for view
    
    * refactor: ListItems and storybook
    
    * style: remove transparentButton
    
    * fix: change name for display
    
    * fix: adjust for youse image icon
    
    * style: adjust styles for design
    
    * feat: adjust menu for displays
    
    * Merge branch 'develop' of
    
    * style: adjust styles for design
    
    * fix: display view navigation
    
    * refactor/style: adjust navigation to mobile and destop
    
    * refactor: remove storys
    
    * refactor: remove action and use simple state
    
    * fix: mobile menu scrolling body
    
    * style: adjust styles to desing
    
    * refactor: change name to lowercase
    
    * refactor: change names and imports

--- app/components/Header/menuData.js
+++ app/components/Header/menuData.js
@@ -1,9 +1,20 @@
+import Image from 'next/image';
+import homeIcon from '/public/home.svg';
+
+const imageIcon = (
+  <>
+    <Image src={homeIcon} alt="union" height={20} width={24} />
+  </>
+);
+
 export const navData = [
-  { id: 1, name: 'home', link: '/' },
-  { id: 2, name: 'tv shows', link: '/' },
+  {
+    id: 1,
+    icon: imageIcon,
+    link: '/',
+  },
+  { id: 2, name: 'favourites', link: '/' },
   { id: 3, name: 'movies', link: '/' },
-  { id: 4, name: 'new & popular', link: '/' },
-  { id: 5, name: 'my list', link: '/' },
+  { id: 4, name: 'series', link: '/' },
+  { id: 5, name: 'kids', link: '/' },
 ];
-export const userImg = ':)';
-export const mobileMenu = 'Explore';
