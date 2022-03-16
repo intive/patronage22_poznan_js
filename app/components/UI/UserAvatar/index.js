3678dafbcb168992fb73101e32fb8165f3752b6f
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2022-03-16 22:56:13 +0100

    chore: cleanup, comment

--- app/components/UI/UserAvatar/index.js
+++ app/components/UI/UserAvatar/index.js
@@ -2,6 +2,7 @@ import { useSession as useSessionActual } from 'next-auth/react';
 import Image from 'next/image';
 import { Avatar } from './UserAvatar.styles';
 
+// useSession prop is passed in stories only to mock user session, do not use in app
 export default function UserAvatar({ size = 40, useSession = useSessionActual }) {
   const { data: session = {} } = useSession();
 
