0f6c3da3467685312eab0970677fcc29b1fc39d9
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-03-16 16:03:20 +0100

    refactor: rename variables and functions from validateFormInputs file

--- app/utils/validateFormInputs.js
+++ app/utils/validateFormInputs.js
@@ -1,18 +1,19 @@
-const emailPattern = /^[a-z\d]+[\w\d.-]*(\+[\w\d.-]*)?@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
-const namePattern = /^[A-Za-z0-9]*$/;
-const passwordPattern = /^\S{6,}$/;
+const emailSignUpPattern =
+  /^[a-z\d]+[\w\d.-]*(\+[\w\d.-]*)?@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
+const nameSignUpPattern = /^[A-Za-z0-9]*$/;
+const passwordSignUpPattern = /^\S{6,}$/;
 
 const validateSignUpFormInputs = (inputValues) => {
   let errorMsg = {};
 
   if ('email' in inputValues) {
-    errorMsg.email = validateUserEmail(inputValues.email);
+    errorMsg.email = validateSignUpUserEmail(inputValues.email);
   }
   if ('name' in inputValues) {
-    errorMsg.name = validateUserName(inputValues.name);
+    errorMsg.name = validateSignUpUserName(inputValues.name);
   }
   if ('password' in inputValues) {
-    errorMsg.password = validateUserPassword(inputValues.password);
+    errorMsg.password = validateSignUpUserPassword(inputValues.password);
   }
 
   return errorMsg;
@@ -20,30 +21,30 @@ const validateSignUpFormInputs = (inputValues) => {
 
 export default validateSignUpFormInputs;
 
-export const validateUserEmail = (emailAdress) => {
+export const validateSignUpUserEmail = (emailAdress) => {
   if (!emailAdress) {
     return 'Email adress is required';
-  } else if (!emailPattern.test(emailAdress)) {
+  } else if (!emailSignUpPattern.test(emailAdress)) {
     return 'Please provide correct email adress';
   }
 
   return '';
 };
 
-export const validateUserName = (name) => {
+export const validateSignUpUserName = (name) => {
   if (!name) {
     return 'Name is required';
-  } else if (!namePattern.test(name)) {
+  } else if (!nameSignUpPattern.test(name)) {
     return 'Name needs to contain letters and digits only';
   }
 
   return '';
 };
 
-export const validateUserPassword = (password) => {
+export const validateSignUpUserPassword = (password) => {
   if (!password) {
     return 'Password is required';
-  } else if (!passwordPattern.test(password)) {
+  } else if (!passwordSignUpPattern.test(password)) {
     return 'Please provide one word min. 6 characters long';
   }
 
