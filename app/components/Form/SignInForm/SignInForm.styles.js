3d96c48d0f68676b500a07642e12ad6c3d2988d3
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-03-17 15:39:08 +0100

    feat: ajust Spinner Button and Error Message

---
+++ app/components/Form/SignInForm/SignInForm.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const GeneralErrorMessage = styled.div`
+  display: flex;
+  background-color: #eb1c1c;
+  color: #fff;
+  margin-top: -20px;
+  margin-bottom: 15px;
+  width: 100%;
+  height: 2rem;
+  border-radius: 15px;
+  justify-content: center;
+  font-size: 1.1rem;
+  align-items: center;
+`;
--- app/components/Form/SignInForm/index.js
+++ app/components/Form/SignInForm/index.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import Button from 'components/UI/Button';
 import Input from '../Input';
 import { FormContainer } from '../Form.styles';
 import { validateSignInForm } from 'utils/validateFormInputs';
+import { ErrorIcon } from '../Input/Input.styles';
+import { GeneralErrorMessage } from './SignInForm.styles';
 
 const initialState = Object.freeze({ email: '', password: '' });
 
@@ -18,37 +20,35 @@ export default function SignInForm() {
   const userLogin = async (e) => {
     e.preventDefault();
     setInputErrors({});
-    setIsFormSubmitting(true);
     setLoginError(false);
     let validationErrors = validateSignInForm(inputValues);
 
     if (Object.values(validationErrors).length > 0) {
       setInputErrors(validationErrors);
     } else {
+      setIsFormSubmitting(true);
       const user = inputValues;
       // It's only temporary console.log:
       console.log(user);
       const res = await signIn('credentials', { ...user, callbackUrl: '/', redirect: false });
-      if (res?.error) {
-        setLoginError(true);
-      } else {
-        setLoginError(false);
-      }
-      if (res.url) {
-        router.push(res.url);
-      }
+      authenticateUser(res);
+      setIsFormSubmitting(false);
     }
   };
-  useEffect(() => {
-    const timer = setTimeout(() => setIsFormSubmitting(false), 300);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [isFormSubmitting]);
+
+  const authenticateUser = (res) => {
+    if (res?.error) {
+      setLoginError(true);
+    } else {
+      setLoginError(false);
+    }
+    if (res.url) {
+      router.push(res.url);
+    }
+  };
 
   return (
     <FormContainer>
-      {loginError && <span style={{ color: 'red' }}>Invalid Credentials</span>}
       <Input
         id="email"
         type="email"
@@ -67,6 +67,12 @@ export default function SignInForm() {
         label="Password"
         error={inputErrors.password}
       />
+      {loginError && (
+        <GeneralErrorMessage>
+          <ErrorIcon type="x-mark" style={{ paddingTop: '2px' }} />
+          <span>Please enter correct login data</span>
+        </GeneralErrorMessage>
+      )}
       <Button fullWidth primary isLoading={isFormSubmitting} onClick={userLogin}>
         Sign in
       </Button>
