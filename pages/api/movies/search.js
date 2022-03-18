cbfcbd45d7ebc3812d6315e5b9566dd2c3e54a59
Piotr Pieczara <56893415+rast-mk1@users.noreply.github.com>
2022-03-18 21:49:20 +0100

    feat: backend movies/search #1743  (#83)
    * feat: add getMovieSearchOutcome function
    
    * feat: add search endpoint
    
    * chore: fix import
    
    * chore: add withAuth for search.js
    
    * fix: forbid undefined query from being sent to client

---
+++ pages/api/movies/search.js
@@ -0,0 +1,39 @@
+import withAuth from 'server/withAuth';
+import { getMovieSearchOutcome } from 'server/services/movieDb';
+
+/**
+ * @swagger
+ * /api/movies/search:
+ *   get:
+ *     summary: Search for movies.
+ *     description: Returns a list of movies by used query
+ *     parameters:
+ *     - in: query
+ *       name: query
+ *       required: true
+ *       description: Pass a text query to search.
+ *     responses:
+ *       200:
+ *         description: list of movies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *       404:
+ *         description: movies not found
+ */
+async function handler(req, res) {
+  try {
+    const { query } = req.query;
+    const movies = await getMovieSearchOutcome(query);
+    if (!movies) {
+      return res.status(404).json();
+    }
+    return res.status(200).json(movies.results);
+  } catch (e) {
+    const message = e.response ? await e.response.text() : e.message;
+    return res.status(400).json(message);
+  }
+}
+
+export default withAuth(handler);
