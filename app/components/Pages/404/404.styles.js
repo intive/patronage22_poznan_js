9105d4072770f0c16f422d48210387c9a022c5a3
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-03-21 23:45:49 +0100

    feat: create 404Page

---
+++ app/components/Pages/404/404.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Wrapper404 = styled.div`
+  background: #2a2535;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 96px 32px;
+  min-height: 100vh;
+`;
+
+export const Message404 = styled.h2`
+  color: #ffffff;
+  font-weight: 500;
+  text-align: center;
+  padding-top: 16px;
+  padding-left: 16px;
+`;
+
+export const ImageWrapper = styled.div`
+  position: relative;
+  width: 90px;
+  height: 50px;
+`;
