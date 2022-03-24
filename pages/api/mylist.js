98e2939a1ddd751fec66c8ee68e708072da6999d
piotr pieczara <piotr.pieczara@gmail.com>
2022-03-24 23:26:38 +0100

    feat: refine GET method for my list

--- pages/api/mylist.js
+++ pages/api/mylist.js
@@ -1,20 +1,13 @@
 import mongoClient from 'server/mongoDb';
 import withAuth from 'server/withAuth';
 
-// import { randomUUID } from 'crypto';
-// import { getPassHash } from 'server/hash';
-// import { validateSignUpUserEmail, validateSignUpUserPassword } from 'utils/validateFormInputs';
-
-export default async function handler(req, res) {
+async function handler(req, res) {
   const client = await mongoClient;
   const db = client.db(process.env.MONGODB_DB);
 
   if (req.method == 'GET') {
-    // TODO zwraca całą listę
-    // const list = db.collection('user_lists').find({});
-    // const list = (await db.collection('user_lists').find({}, { limit: 1 }).toArray())[0];
-    const list = await db.collection('user_lists').find({}).toArray();
-
+    const list = await db.collection('user_lists').findOne({ _id: req.session.id });
     return res.status(200).json(list);
   }
 }
+export default withAuth(handler);
