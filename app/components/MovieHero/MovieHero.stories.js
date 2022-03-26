af4b1754ceeddc46c6deae35f844c91d743db0fd
Konrad Wolniak <kowolniak@gmail.com>
2022-03-26 16:07:59 +0100

    feat: redesign MovieHero comp (#76)
    * feat: redesign MovieHero comp
    
    * refactor: increase font size, fix linear gradient
    
    * refactor: use camelCase and adjust proper IMG path
    
    * refactor: add longer titles proper apperance

---
+++ app/components/MovieHero/MovieHero.stories.js
@@ -0,0 +1,55 @@
+import MovieHero from '.';
+
+export default {
+  title: 'MovieHero/MovieHero',
+  component: MovieHero,
+};
+
+const Template = (args) => <MovieHero movieData={movieMockData} {...args} />;
+
+const movieMockData = {
+  backdrop: {
+    xxs: 'https://image.tmdb.org/t/p/w92/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+    xs: 'https://image.tmdb.org/t/p/w154/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+    s: 'https://image.tmdb.org/t/p/w185/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+    m: 'https://image.tmdb.org/t/p/w342/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+    l: 'https://image.tmdb.org/t/p/w500/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+    xl: 'https://image.tmdb.org/t/p/w780/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+    original: 'https://image.tmdb.org/t/p/original/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg',
+  },
+  genres: [{ id: 18, name: 'Drama' }],
+  popularity: 60.21,
+  poster: {
+    xxs: 'https://image.tmdb.org/t/p/w92/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+    xs: 'https://image.tmdb.org/t/p/w154/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+    s: 'https://image.tmdb.org/t/p/w185/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+    m: 'https://image.tmdb.org/t/p/w342/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+    l: 'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+    xl: 'https://image.tmdb.org/t/p/w780/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+    original: 'https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+  },
+  releaseDate: '1999-10-15',
+  title: 'Fight Club',
+  voteAverage: 8.4,
+};
+
+const dataWithLongTitle = {
+  ...movieMockData,
+  title: "Fight Club but it's very long witch looks strange",
+};
+
+export const Default = Template.bind();
+Default.args = {
+  movieData: movieMockData,
+};
+Default.parameters = {
+  layout: 'fullscreen',
+};
+
+export const LongTitle = Template.bind();
+LongTitle.args = {
+  movieData: dataWithLongTitle,
+};
+LongTitle.parameters = {
+  layout: 'fullscreen',
+};
---
+++ app/components/MovieHero/MovieHero.styles.js
@@ -0,0 +1,69 @@
+import styled, { css } from 'styled-components';
+import { device } from 'consts/mediaQueries';
+
+const HeroWrapper = styled.div`
+  background-image: linear-gradient(180deg, transparent 50%, #1e1b26),
+    url(${(props) => props.backgroundImg});
+  background-size: cover;
+  background-position: center;
+  background-color: #4e564e;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HeroContents = styled.div`
+  color: #fff;
+  text-shadow: 1px 2px 5px black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  position: absolute;
+  bottom: 20%;
+
+  ${device.tablet} {
+    bottom: 35%;
+    align-items: flex-start;
+    margin-left: 15%;
+    max-width: 60%;
+    height: initial;
+  }
+`;
+
+const HeroGenres = styled.span`
+  font-size: 1.15em;
+  margin-bottom: 4px;
+
+  ${device.tablet} {
+    margin-bottom: 8px;
+  }
+`;
+
+const HeroTitle = styled.h1`
+  color: #fff;
+  font-size: 3.5em;
+  margin-bottom: 12px;
+  max-width: 90%;
+  line-height: 1.1;
+  text-align: center;
+  word-wrap: break-word;
+
+  ${device.tablet} {
+    margin-bottom: 24px;
+    text-align: initial;
+  }
+
+  ${({ medium }) =>
+    medium &&
+    css`
+      font-size: 2.75em;
+    `}
+
+  ${({ long }) =>
+    long &&
+    css`
+      font-size: 2.25em;
+    `}
+`;
+
+export { HeroWrapper, HeroGenres, HeroTitle, HeroContents };
