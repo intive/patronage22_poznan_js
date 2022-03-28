6baddc7231964094791c61b3daecf7cbef8a103f
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-03-28 23:20:21 +0200

    feat: add blur image

--- app/components/Pages/AuthPageContainer/index.js
+++ app/components/Pages/AuthPageContainer/index.js
@@ -1,11 +1,20 @@
 import AuthenticationFormHeader from 'components/Form/AuthFormHeader';
 import SignInForm from 'components/Form/SignInForm';
-import { Wrapper, FormContainer } from './AuthPageContainer.styles';
+import { Wrapper, FormContainer, AuthBackground } from './AuthPageContainer.styles';
 import UserSignUpForm from 'components/Form/UserSignUpForm';
 
 export default function AuthenticationPageContainer({ active }) {
   return (
     <Wrapper>
+      <AuthBackground
+        src={'/images/hero-background-image.jpg'}
+        alt="background-image"
+        layout={'fill'}
+        objectFit={'cover'}
+        objectPosition={'top left'}
+        placeholder="blur"
+        blurDataURL={'/images/blur-auth-placeholder.png'}
+      />
       <FormContainer>
         <AuthenticationFormHeader active={active} />
         {active === 'sign-in' ? <SignInForm /> : <UserSignUpForm />}
