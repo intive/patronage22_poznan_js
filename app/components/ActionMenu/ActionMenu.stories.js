3aaf19c49a8f238230613a1b5494e54dac283ec2
wolnio <kowolniak@gmail.com>
2022-03-28 23:28:57 +0200

    refactor: stories changes, correct modal display

--- app/components/ActionMenu/ActionMenu.stories.js
+++ app/components/ActionMenu/ActionMenu.stories.js
@@ -1,21 +1,25 @@
 import ActionMenu from '.';
+import { SessionProvider } from 'next-auth/react';
 
 export default {
   title: 'ActionMenu',
   component: ActionMenu,
-  argTypes: {
-    useSession: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
+};
+
+const mockSession = {
+  user: {
+    name: 'Bruce Wayne',
+    avatar: 1,
+    email: 'batman@test.com',
+    createdAt: '2022-03-05T00:30:17.056+00:00',
+    id: '5a36a064-a1f8-4c29-b941-3109113deac6',
   },
 };
 
-const Template = (args) => <ActionMenu {...args} />;
+const Template = (args) => (
+  <SessionProvider session={mockSession}>
+    <ActionMenu {...args} />
+  </SessionProvider>
+);
 
 export const Default = Template.bind();
-Default.args = {
-  useSession: () => ({ data: { user: { avatar: 8, name: 'Bruce Wayne' } } }),
-};
--- app/components/ActionMenu/index.js
+++ app/components/ActionMenu/index.js
@@ -6,33 +6,35 @@ import Icon from '../UI/Icon';
 import Modal from 'components/Modal';
 import { useActions, openModal } from 'actions/app';
 
-export default function ActionMenu({ useSession = useSessionActual }) {
+export default function ActionMenu({ isOpen, useSession = useSessionActual }) {
   const { data: session } = useSession();
   const state = useActions({ isModalOpen: false, content: '' });
   const { isModalOpen, content } = state;
 
   return (
-    <Wrapper>
-      <UserInfo>
-        {session?.user ? (
-          <>
-            <UserAvatar size={100} />
-            <div className="username">{session.user.name}</div>
-          </>
-        ) : (
-          <>Not signed in :(</>
-        )}
-      </UserInfo>
-      <ActionList>
-        <ActionBtn onlyIcon fullWidth onClick={() => openModal(<h2>Account</h2>)}>
-          Account
-          <Icon size="lg" rotation={270} type="arrow-down" />
-        </ActionBtn>
+    <>
+      <Wrapper>
+        <UserInfo>
+          {session?.user ? (
+            <>
+              <UserAvatar size={100} />
+              <div className="username">{session.user.name}</div>
+            </>
+          ) : (
+            <>Not signed in :(</>
+          )}
+        </UserInfo>
+        <ActionList>
+          <ActionBtn onlyIcon fullWidth onClick={() => openModal(<h2>Account</h2>)}>
+            Account
+            <Icon size="lg" rotation={270} type="arrow-down" />
+          </ActionBtn>
+        </ActionList>
         {isModalOpen && <Modal content={content} />}
-      </ActionList>
-      <Button fullWidth href="#">
-        Log out
-      </Button>
-    </Wrapper>
+        <Button fullWidth href="#">
+          Log out
+        </Button>
+      </Wrapper>
+    </>
   );
 }
