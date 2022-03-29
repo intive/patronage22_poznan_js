1b94ce85569d44f19dec74d9d567bd1b82be7cf1
piotr pieczara <piotr.pieczara@gmail.com>
2022-03-30 00:16:12 +0200

    fix: code improvements and validation

--- pages/api/mylist/[movieId].js
+++ pages/api/mylist/[movieId].js
@@ -1,34 +1,36 @@
 import mongoClient from 'server/mongoDb';
 import withAuth from 'server/withAuth';
 async function handler(req, res) {
+  if (!['put', 'delete'].includes(req.method.toLowerCase())) {
+    return res.status(405).send();
+  }
   const { movieId } = req.query;
   const id = parseInt(movieId, 10);
 
-  if (Number.isInteger(id) && id > 0) {
-    const client = await mongoClient;
-    const db = client.db(process.env.MONGODB_DB);
-    const userId = req.session.id;
-    const query = { user_id: userId };
+  if (isNaN(id) || id < 1) {
+    return res.status(400).json();
+  }
+  const client = await mongoClient;
+  const db = client.db(process.env.MONGODB_DB);
+  const userId = req.session.id;
+  const query = { user_id: userId };
 
-    if (req.method === 'PUT') {
-      const command = { $addToSet: { items: id } };
-      const options = { upsert: true };
-      const userList = await db.collection('user_lists').findOne(query);
-      const numberOfMoviesOnList = userList?.items.length;
-      if (!userList || numberOfMoviesOnList < 50) {
-        db.collection('user_lists').updateOne(query, command, options);
-        return res.status(200).json();
-      } else {
-        return res.status(200).json();
-      }
-    }
-    if (req.method === 'DELETE') {
-      const command = { $pull: { items: id } };
-      db.collection('user_lists').updateOne(query, command);
+  if (req.method === 'PUT') {
+    const command = { $addToSet: { items: id } };
+    const options = { upsert: true };
+    const userList = await db.collection('user_lists').findOne(query);
+    const numberOfMoviesOnList = userList?.items.length;
+    if (!userList || numberOfMoviesOnList < 50) {
+      db.collection('user_lists').updateOne(query, command, options);
+      return res.status(200).json();
+    } else {
       return res.status(200).json();
     }
-  } else {
-    return res.status(400).json();
+  }
+  if (req.method === 'DELETE') {
+    const command = { $pull: { items: id } };
+    db.collection('user_lists').updateOne(query, command);
+    return res.status(200).json();
   }
 }
 export default withAuth(handler);
