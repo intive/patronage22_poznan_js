c2c114ca3faa21fa1b9d91304abddd934ebd81f5
piotr pieczara <piotr.pieczara@gmail.com>
2022-03-29 23:39:21 +0200

    fix: better use of arrow functions and promises

--- app/server/services/myListClient.js
+++ app/server/services/myListClient.js
@@ -13,14 +13,8 @@ export async function getMyList(req) {
   const userList = await db.collection('user_lists').findOne({ user_id: req.session.id });
 
   if (Array.isArray(userList.items)) {
-    const myList = await Promise.all(
-      userList.items.map(async (id) => {
-        return await getMovieById(id);
-      })
-    );
-    const myFilteredList = myList.filter((element) => {
-      return element !== null;
-    });
+    const myList = await Promise.all(userList.items.map((id) => getMovieById(id)));
+    const myFilteredList = myList.filter((element) => element !== null);
     return myFilteredList;
   } else {
     return;
