a8bd418ef923008e18909cf2f2a31870c46125bf
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-03-29 23:58:09 +0200

    fix: authcontainer backgroud size #1784 (#97)
    * fix: smaller backgroundImage
    
    * feat: add blur image
    
    * fix: fix blur

--- app/components/Pages/AuthPageContainer/index.js
+++ app/components/Pages/AuthPageContainer/index.js
@@ -1,11 +1,21 @@
+import backgroundAuthImage from '../../../../public/images/hero-background-image.jpg';
 import AuthenticationFormHeader from 'components/Form/AuthFormHeader';
 import SignInForm from 'components/Form/SignInForm';
-import { Wrapper, FormContainer } from './AuthPageContainer.styles';
+import { Wrapper, FormContainer, AuthBackground } from './AuthPageContainer.styles';
 import UserSignUpForm from 'components/Form/UserSignUpForm';
 
 export default function AuthenticationPageContainer({ active }) {
   return (
     <Wrapper>
+      <AuthBackground
+        src={backgroundAuthImage}
+        alt="background-image"
+        layout={'fill'}
+        objectFit={'cover'}
+        objectPosition={'top left'}
+        placeholder="blur"
+        quality={95}
+      />
       <FormContainer>
         <AuthenticationFormHeader active={active} />
         {active === 'sign-in' ? <SignInForm /> : <UserSignUpForm />}
