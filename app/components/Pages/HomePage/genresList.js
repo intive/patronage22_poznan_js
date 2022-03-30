1ddc64d382ae5d44ee35e24f358456b9e04c5a20
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-03-30 23:30:08 +0200

    feat: home page #1728 (#96)
    * feat: homePage carousels
    
    * feat: homePage improvement
    
    * feat: add MovieHero to HomePage
    
    * style: improvement HomePage styles
    
    * refactor: remove empty lines
    
    * refactor: refactor Hero Movie Img and style
    
    * fix: check image value
    
    * refactor: improvement style and corrections after code review
    
    * refactor: remove alt from movie poster
    
    * refactor: corrections after code review

---
+++ app/components/Pages/HomePage/genresList.js
@@ -0,0 +1,30 @@
+export const genresList = [
+  {
+    id: 35,
+    name: 'Comedy',
+  },
+  {
+    id: 10749,
+    name: 'Romance',
+  },
+  {
+    id: 10751,
+    name: 'Family',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+  },
+  {
+    id: 27,
+    name: 'Horror',
+  },
+  {
+    id: 14,
+    name: 'Fantasy',
+  },
+  {
+    id: 53,
+    name: 'Thriller',
+  },
+];
