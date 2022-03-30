2d9a232efd972ac0231c5b0c5c627ac845135585
Lena Sokolova <sokolhelena94@gmail.com>
2022-03-30 19:27:14 +0200

    refactor: CategoryPage search

--- app/components/Pages/CategoryPage/CategoryPage.js
+++ app/components/Pages/CategoryPage/CategoryPage.js
@@ -3,28 +3,33 @@ import { useState } from 'react';
 import { WrapperGrid, MovieCardWrapper, SearchDiv, SearchStyle } from './CategoryPage.styles';
 
 export function CategoryPage({ moviesData, ...props }) {
-  console.log(moviesData);
   if (moviesData.length === 0) {
     console.alert("Oops looks like there's no movies anymore");
     //need to fix error when the array is empty
   }
 
-  const [filteredData, setFilteredData] = useState('');
-  const handleSearch = (e) => {
-    let value = e.target.value.toLowerCase();
-    let result = [];
-    console.log(value);
-    result = moviesData.filter((data) => {
-      return data.title.search(value) != -1;
-    });
-    setFilteredData(result);
-  };
+  const [searchQ, setSearchQ] = useState('');
+  console.log(searchQ);
 
   return (
     <>
       <SearchDiv>
         <SearchStyle>
-          <input onChange={(e) => handleSearch(e.target.value)} type="text" value={filteredData} />
+          <input
+            onChange={(event) => setSearchQ(event)}
+            {...moviesData
+              .filter((movie) => movie.title.includes(searchQ))
+              .map((film, index) => (
+                <MovieCardWrapper key={index}>
+                  <MovieCard
+                    title={film.title}
+                    src={film.images.poster.m}
+                    alt={film.title}
+                    {...props}
+                  ></MovieCard>
+                </MovieCardWrapper>
+              ))}
+          />
         </SearchStyle>
       </SearchDiv>
       <WrapperGrid>
