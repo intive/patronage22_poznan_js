e44b5835db8b8934dc420ab72fac96548bdff8d8
Lena Sokolova <sokolhelena94@gmail.com>
2022-03-30 09:32:22 +0200

    feat: CategoryPage component

--- app/components/Pages/CategoryPage/CategoryPage.js
+++ app/components/Pages/CategoryPage/CategoryPage.js
@@ -1,20 +1,44 @@
 import MovieCard from 'components/UI/MovieCard';
-import { Wrapper } from './CategoryPage.styles';
-import Link from 'next/link';
+import { useState } from 'react';
+import { WrapperGrid, MovieCardWrapper, SearchDiv, SearchStyle } from './CategoryPage.styles';
 
-export function CategoryPage({ moviesData }) {
-  console.log(moviesData.length);
+export function CategoryPage({ moviesData, ...props }) {
+  console.log(moviesData);
   if (moviesData.length === 0) {
-    console.log("Oops looks like there's no movies anymore");
+    console.alert("Oops looks like there's no movies anymore");
+    //need to fix error when the array is empty
   }
 
+  const [filteredData, setFilteredData] = useState('');
+  const handleSearch = (e) => {
+    let value = e.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = moviesData.filter((data) => {
+      return data.title.search(value) != -1;
+    });
+    setFilteredData(result);
+  };
+
   return (
-    <Wrapper>
-      {moviesData.map((film, index) => (
-        <Link a href={`/movies/category/${film.id}`} prefetch={false} key={index} passHref>
-          <MovieCard title={film.title}></MovieCard>
-        </Link>
-      ))}
-    </Wrapper>
+    <>
+      <SearchDiv>
+        <SearchStyle>
+          <input onChange={(e) => handleSearch(e.target.value)} type="text" value={filteredData} />
+        </SearchStyle>
+      </SearchDiv>
+      <WrapperGrid>
+        {moviesData.map((film, index) => (
+          <MovieCardWrapper key={index}>
+            <MovieCard
+              title={film.title}
+              src={film.images.poster.m}
+              alt={film.title}
+              {...props}
+            ></MovieCard>
+          </MovieCardWrapper>
+        ))}
+      </WrapperGrid>
+    </>
   );
 }
