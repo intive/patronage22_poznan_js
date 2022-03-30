f203ddd4811ad00a38b9b549c13d886c00a2f4d5
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-03-30 21:03:41 +0200

    feat: add logic to Search component

---
+++ app/components/Pages/Search/index.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { MessageForUser, SearchWrapper } from './Search.styles';
+import MovieGrid from 'components/UI/MovieGrid';
+import { fetchWrapper } from 'utils/fetchWrapper';
+
+export default function Search() {
+  const [listOfMovies, setListOfMovies] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [messageForUser, setMessageForUser] = useState('');
+
+  const router = useRouter();
+  const query = router.query.query;
+  const queryValidationPattern = /^[a-z0-9!&'"?\-\s]{1,100}$/i;
+
+  useEffect(() => {
+    if (query) {
+      const sanitaizedQuery = sanitaize(query);
+      if (sanitaizedQuery) {
+        setIsFetching(true);
+        getMovies(sanitaizedQuery);
+      } else {
+        setMessageForUser("Are you sure you're looking for movie title?");
+      }
+    } else {
+      setMessageForUser("Use search bar to find movie you're interested in.");
+    }
+  }, [query]);
+
+  const getMovies = async (query) => {
+    const response = await fetchWrapper.get(`api/movies/search?query=${query}`);
+    handleServerResponse(response, query);
+  };
+
+  const handleServerResponse = (response, query) => {
+    if (response.error) {
+      setMessageForUser(response.error);
+      setListOfMovies([]);
+    }
+    if (response.data) {
+      //TODO: delete the code line below after backend fix
+      const filteredMovies = response.filter((movie) => movie !== null);
+
+      if (filteredMovies.length < 1) {
+        setMessageForUser(`Your search for "${query}" did not have any matches.`);
+      }
+      setListOfMovies(filteredMovies);
+    }
+    setIsFetching(false);
+  };
+
+  const sanitaize = (query) => {
+    if (!query) return null;
+    const decodedQuery = decodeURI(query);
+    const isQueryValid = queryValidationPattern.test(decodedQuery);
+    return isQueryValid ? decodedQuery.trim().replace(/\s/g, '+') : null;
+  };
+
+  return (
+    <SearchWrapper>
+      {listOfMovies.length < 1 && !isFetching ? (
+        <MessageForUser>{messageForUser}</MessageForUser>
+      ) : (
+        <MovieGrid listOfMovies={listOfMovies} />
+      )}
+    </SearchWrapper>
+  );
+}
--- app/components/UI/SearchMoviesInput/index.js
+++ app/components/UI/SearchMoviesInput/index.js
@@ -1,52 +1,94 @@
+import Router from 'next/router';
+import { useRouter } from 'next/router';
 import { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import useOnClickOutside from 'hooks/useOnClickOutside';
 import { SearchMoviesContainer, SearchInput, SearchButton } from './SearchMoviesInput.styles';
 import Button from 'components/UI/Button';
 import Icon from 'components/UI/Icon';
-import PropTypes from 'prop-types';
 
 export default function SearchMoviesInput() {
   const [isExpanded, setExpanded] = useState(false);
   const [searchInputValue, setSearchInputValue] = useState('');
+  const [isThrottling, setIsThrottling] = useState(false);
 
   const inputRef = useRef();
   const containerRef = useRef();
+  const router = useRouter();
 
-  useOnClickOutside(containerRef, () =>
-    searchInputValue ? setExpanded(true) : setExpanded(false)
-  );
+  useEffect(() => {
+    if (isExpanded && !searchInputValue) {
+      inputRef.current.focus();
+    }
+  }, [isExpanded, searchInputValue]);
 
-  const toggleOpenSearchInput = () => {
-    if (searchInputValue) return;
+  useEffect(() => {
+    if (router.pathname !== '/search') {
+      setSearchInputValue('');
+      setExpanded(false);
+    }
+  }, [router.pathname]);
+
+  useEffect(() => {
+    if (isThrottling) {
+      const timer = setTimeout(() => {
+        setIsThrottling(false);
+      }, 400);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [isThrottling]);
+
+  useOnClickOutside(containerRef, () => {
+    searchInputValue ? setExpanded(true) : setExpanded(false);
+  });
+
+  const handleInputIconClick = () => {
+    if (searchInputValue) {
+      updateQuery();
+      return;
+    }
     setExpanded(!isExpanded);
   };
 
-  const handleInputchange = (event) => {
+  const handleInputChange = (event) => {
     const value = event.target.value;
     setSearchInputValue(value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      updateQuery();
+    }
   };
 
   const clearInputValue = () => {
     setSearchInputValue('');
   };
 
-  useEffect(() => {
-    if (isExpanded) {
-      inputRef.current.focus();
+  const updateQuery = () => {
+    setIsThrottling(true);
+    if (searchInputValue.length > 1 && !isThrottling) {
+      Router.push({
+        pathname: '/search',
+        query: { query: searchInputValue },
+      });
     }
-  }, [isExpanded]);
+  };
 
   return (
     <SearchMoviesContainer isExpanded={isExpanded} ref={containerRef}>
-      <SearchButton onlyIcon onClick={toggleOpenSearchInput}>
+      <SearchButton onlyIcon onClick={handleInputIconClick}>
         <Icon type="search" size="lg" />
       </SearchButton>
       {isExpanded && (
         <SearchInput
           isExpanded={isExpanded}
-          placeholder="Seach by movie title"
+          placeholder="Search by movie title"
           value={searchInputValue}
-          onChange={(e) => handleInputchange(e)}
+          onChange={(e) => handleInputChange(e)}
+          onKeyDown={handleKeyDown}
           ref={inputRef}
         ></SearchInput>
       )}
