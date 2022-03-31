11fece0d4d982b113d693d45adc8f4ea6630ec4c
Lena Sokolova <sokolhelena94@gmail.com>
2022-03-31 21:37:07 +0200

    refactor: add filter method & error

---
+++ app/components/Pages/CategoryPage/index.js
@@ -0,0 +1,56 @@
+import MovieCard from 'components/UI/MovieCard';
+import { useState } from 'react';
+import {
+  WrapperGrid,
+  MovieCardWrapper,
+  SearchDiv,
+  SearchStyle,
+  ErrorWrapper,
+  NoMovieError,
+} from './CategoryPage.styles';
+import { Input } from 'components/Form';
+
+export default function CategoryPage({ moviesData, ...props }) {
+  if (moviesData.length === 0) {
+    return (
+      <ErrorWrapper>
+        <NoMovieError>Oops looks like there are no movies in this category! :(</NoMovieError>
+      </ErrorWrapper>
+    );
+  }
+
+  const [searchQ, setSearchQ] = useState('');
+
+  const filteredList = searchQ
+    ? moviesData.filter((movie) => movie.title.includes(searchQ))
+    : moviesData;
+
+  return (
+    <>
+      <SearchDiv>
+        <SearchStyle>
+          <Input
+            id="search"
+            placeholder="Enter the title..."
+            onChange={(event) => setSearchQ(event.target.value)}
+            value={searchQ}
+          />
+        </SearchStyle>
+      </SearchDiv>
+      <WrapperGrid>
+        {filteredList.map((film, id) => (
+          <MovieCardWrapper key={id}>
+            <MovieCard
+              href={`/movie/${props.id}`}
+              movieId={film.id}
+              title={film.title}
+              src={film.images.poster.m}
+              alt={film.title}
+              {...props}
+            ></MovieCard>
+          </MovieCardWrapper>
+        ))}
+      </WrapperGrid>
+    </>
+  );
+}
