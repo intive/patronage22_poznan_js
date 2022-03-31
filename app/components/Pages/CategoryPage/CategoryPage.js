1d3ed7232705919bdcfd562c872901f5985d53e1
Lena Sokolova <sokolhelena94@gmail.com>
2022-03-31 17:55:49 +0200

    refactor: add id and placeholder to the Input

--- app/components/Pages/CategoryPage/CategoryPage.js
+++ app/components/Pages/CategoryPage/CategoryPage.js
@@ -1,12 +1,15 @@
 import MovieCard from 'components/UI/MovieCard';
 import { useState } from 'react';
 import { WrapperGrid, MovieCardWrapper, SearchDiv, SearchStyle } from './CategoryPage.styles';
+import { Input } from 'components/Form';
 
 export function CategoryPage({ moviesData, ...props }) {
   if (moviesData.length === 0) {
     console.alert("Oops looks like there's no movies anymore");
     //need to fix error when the array is empty
   }
+
+  const [isInputActive, setInputActive] = useState(false);
 
   const [searchQ, setSearchQ] = useState('');
   console.log(searchQ);
@@ -15,35 +18,44 @@ export function CategoryPage({ moviesData, ...props }) {
     <>
       <SearchDiv>
         <SearchStyle>
-          <input
-            onChange={(event) => setSearchQ(event)}
-            {...moviesData
-              .filter((movie) => movie.title.includes(searchQ))
-              .map((film, index) => (
-                <MovieCardWrapper key={index}>
-                  <MovieCard
-                    title={film.title}
-                    src={film.images.poster.m}
-                    alt={film.title}
-                    {...props}
-                  ></MovieCard>
-                </MovieCardWrapper>
-              ))}
+          <Input
+            id="search"
+            isInputActive={isInputActive}
+            placeholder="Enter the title..."
+            onChange={(event) => setSearchQ(event.target.value)}
           />
         </SearchStyle>
       </SearchDiv>
-      <WrapperGrid>
-        {moviesData.map((film, index) => (
-          <MovieCardWrapper key={index}>
-            <MovieCard
-              title={film.title}
-              src={film.images.poster.m}
-              alt={film.title}
-              {...props}
-            ></MovieCard>
-          </MovieCardWrapper>
-        ))}
-      </WrapperGrid>
+      {isInputActive && (
+        <WrapperGrid>
+          {moviesData
+            .filter((movie) => movie.title.includes(searchQ))
+            .map((film, index) => (
+              <MovieCardWrapper key={index}>
+                <MovieCard
+                  title={film.title}
+                  src={film.images.poster.m}
+                  alt={film.title}
+                  {...props}
+                ></MovieCard>
+              </MovieCardWrapper>
+            ))}
+        </WrapperGrid>
+      )}
+      {!isInputActive && (
+        <WrapperGrid>
+          {moviesData.map((film, index) => (
+            <MovieCardWrapper key={index}>
+              <MovieCard
+                title={film.title}
+                src={film.images.poster.m}
+                alt={film.title}
+                {...props}
+              ></MovieCard>
+            </MovieCardWrapper>
+          ))}
+        </WrapperGrid>
+      )}
     </>
   );
 }
