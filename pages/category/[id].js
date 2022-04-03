9c20d29db35f7489f1e7381c21366da1f692093a
Lena Sokolova <sokolhelena94@gmail.com>
2022-04-03 13:25:55 +0200

    refactror: change check genre method, add getListOfPopularMovies

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -1,8 +1,14 @@
-import { getListOfMoviesByCategoryId } from 'server/services/movieDb';
+import {
+  getListOfGenres,
+  getListOfMoviesByCategoryId,
+  getListOfPopularMovies,
+} from 'server/services/movieDb';
 import CategoryPage from 'components/Pages/CategoryPage';
 
-export default function Category({ moviesData }) {
-  return <CategoryPage moviesData={moviesData} />;
+export default function Category({ moviesData, categoryList, categoryName }) {
+  return (
+    <CategoryPage moviesData={moviesData} categoryList={categoryList} categoryName={categoryName} />
+  );
 }
 
 export async function getServerSideProps({ params }) {
@@ -10,7 +16,25 @@ export async function getServerSideProps({ params }) {
 
   try {
     const movies = await getListOfMoviesByCategoryId(id);
-    return { props: { moviesData: movies ? movies.results : [] } };
+    if (id === 1) {
+      const popularMovies = await getListOfPopularMovies();
+      const categoryName = 'Popular';
+      return {
+        props: {
+          categoryName,
+          moviesData: popularMovies ? popularMovies.results : [],
+        },
+      };
+    }
+
+    const categoryList = await getListOfGenres();
+    // if (categoryList.find((category) => category.id === id)) {
+    //   const categoryName = categoryList.name;
+    //   return categoryName;
+    // }
+
+    //this above makes moviesData to be undefined :(
+    return { props: { categoryList, moviesData: movies ? movies.results : [] } };
   } catch (e) {
     console.error(e);
     return { props: {} };
