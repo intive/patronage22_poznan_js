09a461747d556c3776418db1cb25e74f43d90916
Lena Sokolova <sokolhelena94@gmail.com>
2022-04-04 18:15:26 +0200

    refactor: change passed props, order of checking category id

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -5,17 +5,14 @@ import {
 } from 'server/services/movieDb';
 import CategoryPage from 'components/Pages/CategoryPage';
 
-export default function Category({ moviesData, categoryList, categoryName }) {
-  return (
-    <CategoryPage moviesData={moviesData} categoryList={categoryList} categoryName={categoryName} />
-  );
+export default function Category({ moviesData, categoryName }) {
+  return <CategoryPage moviesData={moviesData} categoryName={categoryName} />;
 }
 
 export async function getServerSideProps({ params }) {
   const { id } = params;
 
   try {
-    const movies = await getListOfMoviesByCategoryId(id);
     if (id === 1) {
       const popularMovies = await getListOfPopularMovies();
       const categoryName = 'Popular';
@@ -26,15 +23,16 @@ export async function getServerSideProps({ params }) {
         },
       };
     }
-
+    const movies = await getListOfMoviesByCategoryId(id);
     const categoryList = await getListOfGenres();
-    if (categoryList.find((category) => category.id === id)) {
-      const categoryName = categoryList.name;
-      return categoryName;
-    }
-    // this above makes moviesData to be undefined :(
+    const categoryName = categoryList.find((category) => category.id === id);
 
-    return { props: { categoryList, moviesData: movies ? movies.results : [] } };
+    return {
+      props: {
+        categoryName,
+        moviesData: movies ? movies.results : [],
+      },
+    };
   } catch (e) {
     console.error(e);
     return { props: {} };
