a0f006dc96042667035f160d1a293370e1b79d95
Weronika Wąsowska <97474985+weronika299@users.noreply.github.com>
2022-04-06 23:34:10 +0200

    style: remove empty line

--- app/server/services/myListClient.js
+++ app/server/services/myListClient.js
@@ -11,7 +11,6 @@ export async function getMyList(req) {
   const client = await mongoClient;
   const db = client.db(process.env.MONGODB_DB);
   const userList = await db.collection('user_lists').findOne({ user_id: req.session.id });
-
   if (Array.isArray(userList?.items)) {
     const myList = await Promise.all(userList.items.map((id) => getMovieById(id)));
     const myFilteredList = myList.filter((element) => element !== null);
