c3f352fb66cce1dd0b5078d2b31625024685d717
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-04-07 20:33:15 +0200

    fix: fix MovieCard component display in storybook

--- app/components/UI/MovieGrid/index.js
+++ app/components/UI/MovieGrid/index.js
@@ -6,7 +6,7 @@ export default function MovieGrid({ listOfMovies = [] }) {
   return (
     <StyledMovieGrid>
       {listOfMovies.map(({ id, title, images }) => (
-        <MovieCard ratio="1/1.5" key={id} id={id} title={title} src={images?.poster?.l} />
+        <MovieCard key={id} id={id} title={title} src={images?.poster?.l} />
       ))}
     </StyledMovieGrid>
   );
