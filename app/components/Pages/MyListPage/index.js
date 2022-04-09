00254ba3ba73d906c10a7009c3056e27323917d3
Lena Sokolova <sokolhelena94@gmail.com>
2022-04-09 14:39:07 +0200

    feat: add myList page

---
+++ app/components/Pages/MyListPage/index.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import MovieGrid from 'components/UI/MovieGrid';
+import { Input } from 'components/Form';
+import {
+  MyListWrapper,
+  WrapperGrid,
+  SearchDiv,
+  SearchStyle,
+  ErrorWrapper,
+  NoMovieError,
+  TitleWrapper,
+  MyListTitle,
+} from './MyListPage.styles';
+
+export default function MyListPage({ moviesData = [] }) {
+  const [searchQ, setSearchQ] = useState('');
+  console.log(moviesData);
+
+  if (moviesData.length === 0) {
+    return (
+      <ErrorWrapper>
+        <NoMovieError>Oops, looks like there are no movies in your list! :(</NoMovieError>
+      </ErrorWrapper>
+    );
+  }
+  const filteredList = searchQ
+    ? moviesData.filter((movie) => movie.title?.toLowerCase().includes(searchQ.toLowerCase()))
+    : moviesData;
+
+  return (
+    <>
+      <MyListWrapper>
+        <SearchDiv>
+          <SearchStyle>
+            <Input
+              id="search"
+              placeholder="Search the movies in this category..."
+              onChange={(event) => setSearchQ(event.target.value)}
+              value={searchQ}
+            />
+          </SearchStyle>
+        </SearchDiv>
+        <TitleWrapper>
+          <MyListTitle></MyListTitle>
+        </TitleWrapper>
+        <WrapperGrid>
+          <MovieGrid listOfMovies={filteredList} />
+        </WrapperGrid>
+      </MyListWrapper>
+    </>
+  );
+}
---
+++ pages/mylist.js
@@ -0,0 +1,21 @@
+import { getMyList } from 'server/services/myListClient';
+import MyListPage from 'components/Pages/MyListPage';
+
+export default function MyList({ moviesData }) {
+  return <MyListPage moviesData={moviesData} />;
+}
+
+export async function getServerSideProps({ req }) {
+  try {
+    const myList = await getMyList({ req });
+
+    return {
+      props: {
+        moviesData: myList ? myList.results : [],
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    return { props: {} };
+  }
+}
