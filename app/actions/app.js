4d55a495f6b2fadb9dc71580937a0f4a3ecb3e09
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-04-12 19:33:00 +0200

    feat: add actions and extend  app reducer

--- app/actions/app.js
+++ app/actions/app.js
@@ -3,6 +3,10 @@ import withReducerContext from 'actions/middleware/withReducerContext';
 export const actions = {
   CLOSE_MODAL: 'modal-close',
   OPEN_MODAL: 'modal-open',
+  ADD_MOVIE_TO_MY_LIST: 'add-movie-to-my-list',
+  REMOVE_MOVIE_FROM_MY_LIST: 'remove-movie-from-my-list',
+  IS_MOVIE_ON_MY_LIST: 'is-movie-on-my-list',
+  SET_MOVIES_IN_MY_LIST: 'set-movies-in-my-list',
   //
   dispatch: (action) => {
     console.error('Actions was used before dispatch was ready - app store', action);
@@ -20,6 +24,15 @@ export const reducer = (state, action = {}) => {
     case actions.OPEN_MODAL:
       return { ...state, isModalOpen: true, content: payload || '' };
 
+    case actions.ADD_MOVIE_TO_MY_LIST:
+      return { ...state, myList: { ...state.myList, [+payload]: true } };
+
+    case actions.REMOVE_MOVIE_FROM_MY_LIST: {
+      const { [+payload]: removedMovie, ...restMovies } = state.myList;
+      return { ...state, myList: restMovies };
+    }
+    case actions.SET_MOVIES_IN_MY_LIST:
+      return { ...state, myList: payload };
     default:
       break;
   }
@@ -48,4 +61,16 @@ export const closeModal = () => {
  */
 export const openModal = (payload) => {
   actions.dispatch({ type: actions.OPEN_MODAL, payload });
+};
+
+export const addMovieToMyList = (payload) => {
+  actions.dispatch({ type: actions.ADD_MOVIE_TO_MY_LIST, payload });
+};
+
+export const removeMovieFromMyList = (payload) => {
+  actions.dispatch({ type: actions.REMOVE_MOVIE_FROM_MY_LIST, payload });
+};
+
+export const setMoviesInMyList = (payload) => {
+  actions.dispatch({ type: actions.SET_MOVIES_IN_MY_LIST, payload });
 };
