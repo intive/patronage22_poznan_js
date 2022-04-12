949ecae6616c6d8700c487dfcf7302a85b9ee70d
marcelina-hasiak <marcelina.hasiak@gmail.com>
2022-04-12 19:35:22 +0200

    feat: style LikeButton parent components

--- app/components/Carousel/Carousel.styles.js
+++ app/components/Carousel/Carousel.styles.js
@@ -36,7 +36,7 @@ export const CarouselItemInner = styled.div`
   background-color: transparent;
   text-align: center;
   border-radius: 5px;
-  cursor: pointer;
+  /* cursor: pointer; */
 `;
 
 export const NavButton = styled.button`
