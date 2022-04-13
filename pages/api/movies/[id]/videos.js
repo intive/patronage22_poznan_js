4e810564630babd912c4b745e1a7f3a4569c2af7
piotr pieczara <piotr.pieczara@gmail.com>
2022-04-14 00:19:53 +0200

    refactor: narrow return down to results

--- pages/api/movies/[id]/videos.js
+++ pages/api/movies/[id]/videos.js
@@ -4,11 +4,11 @@ import { getVideosByMovieId } from 'server/services/movieDb';
 async function handler(req, res) {
   try {
     const { id } = req.query;
-    const movies = await getVideosByMovieId(id);
-    if (!movies) {
+    const videos = await getVideosByMovieId(id);
+    if (!videos) {
       return res.json([]);
     }
-    return res.status(200).json(movies);
+    return res.status(200).json(videos.results);
   } catch (e) {
     const message = e.response ? await e.response.text() : e.message;
     return res.status(400).json(message);
