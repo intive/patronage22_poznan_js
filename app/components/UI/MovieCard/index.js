a4f8967ff30e1edc83de1fc0583c1107946b9430
Marcela <62512837+marcelina-hasiak@users.noreply.github.com>
2022-05-04 13:11:06 +0200

    feat: add MyList button #1901  (#120)
    * feat: add LikeButton component
    
    * feat: add actions and extend  app reducer
    
    * feat: extend fetchWrapper with put and delete fetch method
    
    * feat: add and handle myList from serverSideProps
    
    * feat: style LikeButton parent components
    
    * style: remove comments
    
    * feat: add storybook-react-context package
    
    Co-authored-by: Wojciech Lechowicz <wojciech.lechowicz@intive.com>

--- app/components/UI/MovieCard/index.js
+++ app/components/UI/MovieCard/index.js
@@ -1,27 +1,36 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import MoviePoster from 'components/UI/MoviePoster';
-import { Wrapper, PosterWrapper, ImagePlaceholder, TitleWrapper, Title } from './MovieCard.styles';
+import {
+  Wrapper,
+  PosterWrapper,
+  ImagePlaceholder,
+  TitleAndLikeButtonWrapper,
+  Title,
+} from './MovieCard.styles';
+import LikeButton from 'components/LikeButton';
 
 export default function MovieCard({ ...props }) {
   return (
-    // I prepared movieID for future use
-    <Link href={`/movie/${props.id}`} movieId={props.id}>
-      <a>
-        <Wrapper>
-          <PosterWrapper ratio={props.ratio}>
-            {props.src ? (
-              <MoviePoster {...props}></MoviePoster>
-            ) : (
-              <ImagePlaceholder>Image not found</ImagePlaceholder>
-            )}
-          </PosterWrapper>
-          <TitleWrapper>
-            <Title>{props.title}</Title>
-          </TitleWrapper>
-        </Wrapper>
-      </a>
-    </Link>
+    <div>
+      <Link href={`/movie/${props.id}`} movieId={props.id}>
+        <a>
+          <Wrapper>
+            <PosterWrapper ratio={props.ratio}>
+              {props.src ? (
+                <MoviePoster {...props}></MoviePoster>
+              ) : (
+                <ImagePlaceholder>Image not found</ImagePlaceholder>
+              )}
+            </PosterWrapper>
+          </Wrapper>
+        </a>
+      </Link>
+      <TitleAndLikeButtonWrapper>
+        <Title>{props.title}</Title>
+        <LikeButton movieId={props.id} />
+      </TitleAndLikeButtonWrapper>
+    </div>
   );
 }
 
